ff446783ce6ef76bd6ab6cfb9af80aa3edcee36d
Bill Snapper <bill@snapper.net>
2014-01-24 10:49:42 -0500

    Results of exercises from sec 10.5

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -42,4 +42,71 @@ describe "Micropost pages" do
       end
     end
   end
+
+  # Test from section 10.5, exercise 1
+  describe "micopost content" do
+
+    describe "shows 0 microposts" do
+      before do
+        visit root_path
+      end
+      it { should have_content("0 microposts") }
+    end
+
+    describe "shows 1 micropost" do
+      before do
+        FactoryGirl.create(:micropost, user: user, created_at: 1.day.ago)
+        visit root_path
+      end
+      it { should have_content("1 micropost") }
+    end
+
+    describe "shows multiple microposts" do
+      before do
+        FactoryGirl.create(:micropost, user: user, created_at: 1.day.ago)
+        FactoryGirl.create(:micropost, user: user, created_at: 1.hour.ago)
+        FactoryGirl.create(:micropost, user: user, created_at: 1.minute.ago)
+        visit root_path 
+      end
+      it { should have_content("#{user.microposts.count} microposts") }
+    end
+
+  end
+
+  # Test from section 10.5, exercise 2
+  describe "micropost pagination on home page" do
+    before do 
+      31.times { FactoryGirl.create(:user) }
+      31.times { FactoryGirl.create(:micropost, user: user) }
+      visit root_path
+    end
+    it { should have_selector('div.pagination') }
+  end
+
+  # Test from section 10.5, exercise 4
+  describe "no delete link for content created by other" do
+    before do
+      @user2 = FactoryGirl.create(:user)
+      FactoryGirl.create(:micropost, user: user, created_at: 1.day.ago)
+      FactoryGirl.create(:micropost, user: @user2, created_at: 1.day.ago)
+    end
+
+    describe "case 1, no links for user in when user2 visits their page" do
+      before do
+        sign_in @user2
+       visit "/users/#{user.id}"
+     end
+      it { should_not have_link('delete') }
+    end
+
+    describe "case 2, no links for user2 in when user visits their page" do
+      before do
+        sign_in user
+        visit "/users/#{@user2.id}"
+      end
+      it { should_not have_link('delete') }
+    end
+
+  end
+
 end
