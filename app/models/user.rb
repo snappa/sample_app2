a26c96075b71867d934d503e6a1452a6d0aa4b33
Bill Snapper <bill@snapper.net>
2014-01-23 19:44:06 -0500

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,8 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  has_many :microposts, dependent: :destroy
+
   # Make sure email addresses are ALL stored lower case as well as unique.
   before_save { email.downcase! }
   before_save :create_remember_token
@@ -29,6 +31,11 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
+  
   private
 
   def create_remember_token
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -84,6 +84,7 @@ describe "AuthenticationPages" do
             end
           end
         end
+
       end
 
       describe "in the Users controller" do
@@ -103,6 +104,19 @@ describe "AuthenticationPages" do
           it { should have_selector('title', text: 'Sign in') }
         end
 
+      end
+
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { response.should redirect_to(signin_path) }
+        end
       end
     end
 
